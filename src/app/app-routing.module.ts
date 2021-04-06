2a9eb2fb23c47f19f2a5df3e360140481330999e
Jaime Milne <jaime.milne@trademe.co.nz>
2021-04-06 16:42:22 +1200

    added a minute amount of styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { OperatorComponent } from './operator/operator.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'operator',
+    component: OperatorComponent
+  },
+  {
+    path: '**',
+    component: MainComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { OperatorComponent } from './operator/operator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    OperatorComponent
   ],
   imports: [
     BrowserModule,
